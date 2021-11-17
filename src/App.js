30abe016b4a71cd54d3c2993187866c3affb45c9
Eneka Fateen <eftn@outlook.com>
2021-11-17 06:50:53 -0600

    g2g

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import MadLibArea from './MadLibArea';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Madlibs!</h1>
+      <MadLibArea />
+    </div>
+  );
+}
+
+export default App;
